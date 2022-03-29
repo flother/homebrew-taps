cd81455daae955b0c5a21bcc0b9ac43fedd4ac63
Matt Riggott <matt.riggott@gmail.com>
2022-03-29 20:45:14 +0000

    Add formula for Spreet v0.2.0

---
+++ spreet.rb
@@ -0,0 +1,35 @@
+class Spreet < Formula
+  desc "Create a spritesheet from a set of SVG images"
+  homepage "https://github.com/flother/spreet"
+  license "MIT"
+  version "0.2.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-x86_64-apple-darwin.tar.gz"
+      sha256 "125e85fd8e71649552047d7bb68dfebd19f3891265331b532c6a2b941cb9baf0"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-aarch64-apple-darwin.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "c8d54b0bbfe2f1ac39a96d4cf02ece5e826b98fe99e58bb69e4db62e80d7876d"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-arm-unknown-linux-gnueabihf.tar.gz"
+      sha256 "8b17215aac1bb614d9842568e51f53740439dc5986b3b32f7955c79a1b9fd668"
+    end
+  end
+
+  def install
+    bin.install "spreet"
+  end
+
+  test do
+    system "#{bin}/spreet", "--version"
+  end
+end
