f2bc71e4bd0dc3bfa79357078142db1743b74fe9
Matt Riggott <matt.riggott@gmail.com>
2023-02-26 22:34:34 +0000

    Update formula for Spreet v0.7.0

--- spreet.rb
+++ spreet.rb
@@ -1,27 +1,27 @@
 class Spreet < Formula
   desc "Create a spritesheet from a set of SVG images"
   homepage "https://github.com/flother/spreet"
-  version "0.6.0"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-x86_64-apple-darwin.tar.gz"
-      sha256 "cd47e7fb33e757f4d8d489f6ec59b2c6893a833cbd95c06521a584d4413da211"
+      sha256 "f1feacdefd9bbfb082ec945e1c83680330b7d9c0b06e9119f152652f162e4089"
     elsif Hardware::CPU.arm?
       url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-aarch64-apple-darwin.tar.gz"
-      sha256 "95bd2675580ded082ac8654fb653bcf08a3d22bc7f77b0a7f8aebe8e5f62d613"
+      sha256 "bf312a8203057fc1c010a7d77e0e666810dafbd01132ef8eeec3692d527debc3"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "0d9a125052449da9a6b39ef3d85c2a65f628505bead36bfbfc7cb4e03417a94b"
+      sha256 "6fc9800064949bd034975229ba7b75d2584ec7a3d471906b7daa1f8039b476d1"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/flother/spreet/releases/download/v#{version}/spreet-arm-unknown-linux-gnueabihf.tar.gz"
-      sha256 "a6669a23ddbca0014c4256f57af0c79cf4323776e57ae4f614516a3848f4564b"
+      sha256 "1ef8620e6b7795be80bd728eb5cb7bbdf37a81f032b5c894cf76c8f3abf8b6f7"
     end
   end
 
